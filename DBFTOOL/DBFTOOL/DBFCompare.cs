c99868365db5ee55c6e17643483089c04fc8a347
cityluo <xiaocheng_luo@hotmail.com>
2016-07-01 16:18:39 +0800

    dbftool

---
+++ DBFTOOL/DBFTOOL/DBFCompare.cs
@@ -0,0 +1,237 @@
+﻿using dbfcomp;
+using HSUCF.Controls;
+using Hundsun.Framework.BizControls.BaseControl.Export;
+using Hundsun.Framework.BizControls.BaseControl.Print;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBFTOOL
+{
+    public partial class DBFCompare : Form
+    {
+        public DBFCompare()
+        {
+            InitializeComponent();
+            btnImport.SetValidateGroup(this.txtFileName1, "group");
+            btnImport.SetValidateGroup(this.txtFileName2, "group");
+            //btnExport.Exporter.Source = gctlDBFTool.DataSource;
+            btnExport.Exporter = new GridControlExporter(gctlDBFTool);
+            btnPrint.Printer = new GirdControlPrinter(gctlDBFTool);
+        }
+
+        private const decimal StandDiff = 0.005M;
+
+        DBFAnalysis dbfanalysis = new DBFAnalysis();
+        
+        //DataTable tableSource = null;
+
+        List<DBFINFO> sourceList = new List<DBFINFO>();
+
+        private void Method()
+        {
+            if (File.Exists(txtFileName1.Value.Trim()) == false)
+            {
+                MsgBoxUtility.ShowTips("文件1不存在");
+                return;
+            }
+            if (File.Exists(txtFileName2.Value.Trim()) == false)
+            {
+                MsgBoxUtility.ShowTips("文件2不存在");
+                return;
+            }
+            string file1 = txtFileName1.Value.Trim();
+            string file2 = txtFileName2.Value.Trim();
+            //string file1 = @"C:\Users\yangzq13148\Desktop\导入\20160615.dbf";
+            //string file2 = @"C:\Users\yangzq13148\Desktop\导入\201606151.dbf";
+            DataTable tableSource = dbfanalysis.GetCompTable(file1, file2);
+            sourceList = ConvertTo(tableSource);
+            Fill();
+            //sourceList = list.ToList();
+            //gctlDBFTool.DataSource = sourceList;
+        }
+
+        private void Fill()
+        {
+            List<DBFINFO> list =
+            sourceList.FindAll(p =>
+
+                (chbSame.Checked && (
+                Math.Abs(p.DQXJYE_DIFF) <= StandDiff
+             && Math.Abs(p.QCXJYE_DIFF) <= StandDiff
+             && Math.Abs(p.T0JYKY_ZCD_DIFF) <= StandDiff
+             && Math.Abs(p.T0ZLKY_NCD_DIFF) <= StandDiff
+             && Math.Abs(p.T0ZLKY_ZCD_DIFF) <= StandDiff
+             && Math.Abs(p.T1JYKY_ZCD_DIFF) <= StandDiff
+             && Math.Abs(p.T1ZLKY_ZCD_DIFF) <= StandDiff
+             )) ||
+                (chbDiff.Checked && (
+                Math.Abs(p.DQXJYE_DIFF) > StandDiff
+             || Math.Abs(p.QCXJYE_DIFF) > StandDiff
+             || Math.Abs(p.T0JYKY_ZCD_DIFF) > StandDiff
+             || Math.Abs(p.T0ZLKY_NCD_DIFF) > StandDiff
+             || Math.Abs(p.T0ZLKY_ZCD_DIFF) > StandDiff
+             || Math.Abs(p.T1JYKY_ZCD_DIFF) > StandDiff
+             || Math.Abs(p.T1ZLKY_ZCD_DIFF) > StandDiff
+                ))
+            );
+
+            gctlDBFTool.DataSource = list;
+        }
+
+        private void chbSame_CheckedChanged(object sender, EventArgs e)
+        {
+            //DataTable table = null;
+            Fill();
+        }
+
+        private void chbDiff_CheckedChanged(object sender, EventArgs e)
+        {
+            Fill();
+        }
+
+        #region DataTable转换为List
+
+        private List<DBFINFO> ConvertTo(DataTable table)
+        {
+            if (table == null)
+            {
+                return null;
+            }
+
+            List<DBFINFO> returnList = new List<DBFINFO>();
+            
+
+            foreach (DataRow row in table.Rows)
+            {
+                DBFINFO obj = new DBFINFO();
+                obj.RQ = Convert.ToInt32(row["RQ"]);
+                obj.JJBH = Convert.ToString(row["JJBH"]);
+                obj.JJMC = Convert.ToString(row["JJMC"]);
+                obj.ZCDYDM = Convert.ToString(row["ZCDYDM"]);
+                obj.ZCDYMC = Convert.ToString(row["ZCDYMC"]);
+                obj.JJDM = Convert.ToString(row["JJDM"]);
+                obj.ZCDYBH = Convert.ToString(row["ZCDYBH"]);
+                obj.DQXJYE1 = Convert.ToDecimal(row["DQXJYE1"]);
+                obj.DQXJYE2 = Convert.ToDecimal(row["DQXJYE2"]);
+                obj.DQXJYE_DIFF = Convert.ToDecimal(row["DQXJYE_DIFF"]);
+                obj.QCXJYE1 = Convert.ToDecimal(row["QCXJYE1"]);
+                obj.QCXJYE2 = Convert.ToDecimal(row["QCXJYE2"]);
+                obj.QCXJYE_DIFF = Convert.ToDecimal(row["QCXJYE_DIFF"]);
+                obj.T0JYKY_ZCD1 = Convert.ToDecimal(row["T0JYKY_ZCD1"]);
+                obj.T0JYKY_ZCD2 = Convert.ToDecimal(row["T0JYKY_ZCD2"]);
+                obj.T0JYKY_ZCD_DIFF = Convert.ToDecimal(row["T0JYKY_ZCD_DIFF"]);
+                obj.T1JYKY_ZCD1 = Convert.ToDecimal(row["T1JYKY_ZCD1"]);
+                obj.T1JYKY_ZCD2 = Convert.ToDecimal(row["T1JYKY_ZCD2"]);
+                obj.T1JYKY_ZCD_DIFF = Convert.ToDecimal(row["T1JYKY_ZCD_DIFF"]);
+                obj.T0ZLKY_ZCD1 = Convert.ToDecimal(row["T0ZLKY_ZCD1"]);
+                obj.T0ZLKY_ZCD2 = Convert.ToDecimal(row["T0ZLKY_ZCD2"]);
+                obj.T0ZLKY_ZCD_DIFF = Convert.ToDecimal(row["T0ZLKY_ZCD_DIFF"]);
+                obj.T0ZLKY_NCD1 = Convert.ToDecimal(row["T0ZLKY_NCD1"]);
+                obj.T0ZLKY_NCD2 = Convert.ToDecimal(row["T0ZLKY_NCD2"]);
+                obj.T0ZLKY_NCD_DIFF = Convert.ToDecimal(row["T0ZLKY_NCD_DIFF"]);
+                obj.T1ZLKY_ZCD1 = Convert.ToDecimal(row["T1ZLKY_ZCD1"]);
+                obj.T1ZLKY_ZCD2 = Convert.ToDecimal(row["T1ZLKY_ZCD2"]);
+                obj.T1ZLKY_ZCD_DIFF = Convert.ToDecimal(row["T1ZLKY_ZCD_DIFF"]);
+                returnList.Add(obj);
+            }
+
+            return returnList;
+        }
+
+        #endregion
+
+        
+        #region DBF class
+
+        public class DBFINFO
+        {
+            public Int32 RQ { get; set; }
+            public String JJBH { get; set; }
+            public String JJMC { get; set; }
+            public String ZCDYDM { get; set; }
+            public String ZCDYMC { get; set; }
+            public String JJDM { get; set; }
+            public String ZCDYBH { get; set; }
+            public Decimal DQXJYE1 { get; set; }
+            public Decimal DQXJYE2 { get; set; }
+            public Decimal DQXJYE_DIFF { get; set; }
+            public Decimal QCXJYE1 { get; set; }
+            public Decimal QCXJYE2 { get; set; }
+            public Decimal QCXJYE_DIFF { get; set; }
+            public Decimal T0JYKY_ZCD1 { get; set; }
+            public Decimal T0JYKY_ZCD2 { get; set; }
+            public Decimal T0JYKY_ZCD_DIFF { get; set; }
+            public Decimal T1JYKY_ZCD1 { get; set; }
+            public Decimal T1JYKY_ZCD2 { get; set; }
+            public Decimal T1JYKY_ZCD_DIFF { get; set; }
+            public Decimal T0ZLKY_ZCD1 { get; set; }
+            public Decimal T0ZLKY_ZCD2 { get; set; }
+            public Decimal T0ZLKY_ZCD_DIFF { get; set; }
+            public Decimal T0ZLKY_NCD1 { get; set; }
+            public Decimal T0ZLKY_NCD2 { get; set; }
+            public Decimal T0ZLKY_NCD_DIFF { get; set; }
+            public Decimal T1ZLKY_ZCD1 { get; set; }
+            public Decimal T1ZLKY_ZCD2 { get; set; }
+            public Decimal T1ZLKY_ZCD_DIFF { get; set; }
+
+        }
+
+        #endregion
+
+        private void hsButton3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog(this) == DialogResult.OK)
+            {
+                txtFileName1.Value = file.FileName;
+            }
+        }
+
+        private void hsButton4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog(this) == DialogResult.OK)
+            {
+                txtFileName2.Value = file.FileName;
+            }
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            //Method();
+            ExcelTOOL.Public.PingAnExcelAnalysis s = new ExcelTOOL.Public.PingAnExcelAnalysis();
+            List<ExcelTOOL.Public.EXCELINFO> list = s.CompExcelFileUsingTable(
+                s.ReadExcelFileToTable(@"C:\Users\Administrator.WINDOWS-TCNHOG0\Desktop\tmp\dbf\综合信息查询_账户资产30.xls"),
+                s.ReadExcelFileToTable(@"C:\Users\Administrator.WINDOWS-TCNHOG0\Desktop\tmp\dbf\综合信息查询_账户资产30t.xls"));
+
+            list.Add(list[1]);
+            Method();
+        }
+        
+        private void bgvDBFTool_RowCellStyle(object sender, DevExpress.XtraGrid.Views.Grid.RowCellStyleEventArgs e)
+        {
+            if (e.CellValue == null)
+                return;
+            string[] containList = { "DQXJYE_DIFF", "QCXJYE_DIFF","T0JYKY_ZCD_DIFF","T1JYKY_ZCD_DIFF","T0ZLKY_ZCD_DIFF","T0ZLKY_NCD_DIFF","T1ZLKY_ZCD_DIFF" };
+            if (containList.Contains<string>(e.Column.Name) == true && e.CellValue.ToString() != "")
+            {
+                decimal data = Convert.ToDecimal(e.CellValue);
+
+                if (Math.Abs(data) > StandDiff)
+                {
+                    e.Appearance.ForeColor = Color.Red;
+                }
+            }
+        }
+
+    }
+}
---
+++ DBFTOOL/DBFTOOL/Public/PingAnExcelAnalysis.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Microsoft.Office.Interop.Excel;
+using System.Collections;
+
+namespace ExcelTOOL.Public
+{
+    public class EXCELINFO
+    {
+        public string A { get; set; }
+        public Decimal B1 { get; set; }
+        public Decimal C1 { get; set; }
+        public Decimal D1 { get; set; }
+        public Decimal E1 { get; set; }
+        public Decimal F1 { get; set; }
+
+        public Decimal B2 { get; set; }
+        public Decimal C2 { get; set; }
+        public Decimal D2 { get; set; }
+        public Decimal E2 { get; set; }
+        public Decimal F2 { get; set; }
+
+        public Decimal B_Diff { get; set; }
+        public Decimal C_Diff { get; set; }
+        public Decimal D_Diff { get; set; }
+        public Decimal E_Diff { get; set; }
+        public Decimal F_Diff { get; set; }
+
+        public EXCELINFO(string s, List<Decimal> list)
+        {
+            int i = 0;
+            this.A = s;
+            this.B1 = list[i++];
+            this.C1 = list[i++];
+            this.D1 = list[i++];
+            this.E1 = list[i++];
+            this.F1 = list[i++];
+            this.B2 = list[i++];
+            this.C2 = list[i++];
+            this.D2 = list[i++];
+            this.E2 = list[i++];
+            this.F2 = list[i++];
+            this.compare();
+        }
+        private void compare()
+        {
+            this.B_Diff = this.B1 - this.B2;
+            this.C_Diff = this.C1 - this.C2;
+            this.D_Diff = this.D1 - this.D2;
+            this.E_Diff = this.E1 - this.E2;
+            this.F_Diff = this.F1 - this.F2;
+        }
+    }
+    public class PingAnExcelAnalysis
+    {
+        /// <summary>
+        /// 读取Excel文件工作簿到DataTable，文件格式为平安信托综合信息查询导出Excel
+        /// </summary>
+        /// <param name="filename">文件路径</param>
+        /// <param name="ws_index">工作簿，默认为第一个</param>
+        /// <returns></returns>
+        public Dictionary<string, List<Decimal>> ReadExcelFileToTable(string filename, int ws_index = 1)
+        {
+            System.Diagnostics.Process[] procs_old = System.Diagnostics.Process.GetProcessesByName("excel");
+            object missing = System.Reflection.Missing.Value;
+            Application excel = new Application();
+            if (excel == null)
+            {
+                return null;
+            }
+
+            // 以只读的形式打开EXCEL文件
+            excel.Visible = false; 
+            excel.UserControl = true;
+            Workbook wb = excel.Application.Workbooks.Open(filename, missing, true, missing, missing, missing, missing, missing, missing, true, missing, missing, missing, missing, missing);
+
+            Worksheet ws = (Worksheet)wb.Worksheets.get_Item(ws_index);        //取得第一个工作薄
+            int rowsint = ws.UsedRange.Cells.Rows.Count;                       //得到行数    (包括标题列)
+
+            Range rngA = ws.Cells.get_Range("A2", "A" + (rowsint - 1));        //取得数据范围区域 (不包括标题列) 最后一行为汇总行，剔除
+            Range rngB = ws.Cells.get_Range("B2", "B" + (rowsint - 1));        
+            Range rngC = ws.Cells.get_Range("C2", "C" + (rowsint - 1));       
+            Range rngD = ws.Cells.get_Range("D2", "D" + (rowsint - 1));        
+            Range rngE = ws.Cells.get_Range("E2", "E" + (rowsint - 1));        
+            Range rngF = ws.Cells.get_Range("F2", "F" + (rowsint - 1));
+
+            object[,] arryA = (object[,])rngA.Value2;
+            object[,] arryB = (object[,])rngB.Value2;
+            object[,] arryC = (object[,])rngC.Value2;
+            object[,] arryD = (object[,])rngD.Value2;
+            object[,] arryE = (object[,])rngE.Value2;
+            object[,] arryF = (object[,])rngF.Value2;
+
+            Dictionary<string, List<Decimal>> dict = new Dictionary<string, List<Decimal>>();
+
+            for (int i = 1; i <= rowsint - 1 - 1; i++)
+            {
+                dict.Add(arryA[i, 1].ToString(), new List<Decimal>{ Convert.ToDecimal(arryB[i, 1].ToString()),
+                                                                    Convert.ToDecimal(arryC[i, 1].ToString()),
+                                                                    Convert.ToDecimal(arryD[i, 1].ToString()),
+                                                                    Convert.ToDecimal(arryE[i, 1].ToString()),
+                                                                    Convert.ToDecimal(arryF[i, 1].ToString())});
+            }
+            excel.Quit(); 
+            excel = null;
+
+            System.Diagnostics.Process[] procs = System.Diagnostics.Process.GetProcessesByName("excel");
+            foreach (System.Diagnostics.Process pro in procs)
+            {
+                bool isexist = false;
+                foreach (System.Diagnostics.Process pro_old in procs_old)
+                {
+                    if (pro.Id == pro_old.Id)
+                    {
+                        isexist = true;
+                        break;
+                    }
+                }
+                if (!isexist)
+                    pro.Kill();//没有更好的方法,只有杀掉进程
+            }
+
+            return dict;
+        }
+
+        public List<EXCELINFO> CompExcelFileUsingTable(Dictionary<string, List<Decimal>> d1, Dictionary<string, List<Decimal>> d2)
+        {
+            List<EXCELINFO> list = new List<EXCELINFO>();
+            Dictionary<string, List<Decimal>> dict_diff = new Dictionary<string, List<Decimal>>(d1);
+            foreach (string s in d2.Keys)
+            {
+                if (dict_diff.ContainsKey(s))
+                {
+                    dict_diff[s].AddRange(d2[s] as List<Decimal>);
+                }
+                else
+                {
+                    for (int i = 0; i < 5; i++)  //前五个元素置空
+                    {
+                        dict_diff[s].Add(0);
+                    }
+                    dict_diff[s].AddRange(d2[s] as List<Decimal>);
+                }
+            }
+            foreach (string s in dict_diff.Keys)
+            {
+                list.Add(new EXCELINFO(s, dict_diff[s]));
+            }
+            dict_diff = null;
+            return list;
+        }
+    }
+}
